26ae556a7e516ae55cc7732128548461ff4b844c
Fabricio Guimaraes de Lima <fabricioglimadev@gmail.com>
2025-07-12 19:38:08 -0300

    Complementando o enunciado do problema

--- src/Exercicio003.java
+++ src/Exercicio003.java
@@ -2,7 +2,8 @@
 Exercício 1.12.3.
 A conversão de graus Farenheit para centígrados é obtida por
 C = 5/9 * (F - 32)
-Fazer um algoritmo que calcule e escreva uma
+Fazer um algoritmo que calcule e escreva uma tabela de centígrados em função de graus
+Farenheit, que variam de 50 a 150 de 1 em 1.
 
 Fonte: Livro Algoritmos Estruturados – Harry Farrer
 
