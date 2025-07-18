8b2126e1a99d0bec5b6c5e6c21a312c1795e0f24
Fabricio Guimaraes de Lima <fabricioglimadev@gmail.com>
2025-07-17 21:11:53 -0300

    Formula ajustada

--- src/Exercicio005.java
+++ src/Exercicio005.java
@@ -21,8 +21,8 @@ public class Exercicio005 {
     int anos = 0;
 
     while (populacaoB <= populacaoA){
-      populacaoA = populacaoA * (1 + taxaA / 100.0);
-      populacaoB = populacaoB * (1 + taxaB / 100.0);
+      populacaoA *= (1 + taxaA / 100.0);
+      populacaoB *= (1 + taxaB / 100.0);
       anos++;
     }
 
